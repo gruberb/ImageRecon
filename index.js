1900d14d2419b02953c633af7c3b370ffdfd7252
Bastian Gruber <gruberbastian@me.com>
2016-12-08 17:25:02 +0100

    Add co for synchronous processing, adjust console.log

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
-const vision = require('node-cloud-vision-api')
+const vision = require('node-cloud-vision-api');
+const co = require('co');
 
 // init with auth
 vision.init({auth: process.env.GOOGLE_API_KEY});
 
 const args = process.argv.slice(2);
-console.log(args[0]);
 // construct parameters
 const req = new vision.Request({
   image: new vision.Image(args[0]),
@@ -13,12 +13,17 @@ const req = new vision.Request({
     new vision.Feature('FACE_DETECTION', 4),
     new vision.Feature('LABEL_DETECTION', 10),
   ]
-})
+});
 
-// send single request
-vision.annotate(req).then((res) => {
-  // handling response
-  console.log(JSON.stringify(res.responses))
-}, (e) => {
-  console.log('Error: ', e)
-})
+co(function* () {
+  let res = yield vision.annotate(req);
+  return res;
+}).then(function (value) {
+    console.log(value);
+    value.responses[0].labelAnnotations.forEach((item) => {
+        console.log(item.description, item.score + ' \n');
+    });
+}, function (err) {
+  console.error(err.stack);
+});
+
